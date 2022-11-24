a57413b70bf539979b555a7c5bc2daf20c5b21e0
Mac Nisbet <mfnisbet@lakeheadu.ca>
2022-11-24 17:01:39 -0500

    Added new tracks
    Added 50 new tracks.

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ from generate_dataframe import *
 from batch_preprocess import *
 
 # Process all the audio files then generate a dataframe
+# Don't need to do this every time
 batch_preprocess()
+# Generates the dataframe
 df = generate_dataframe()
+# Don't need the first value since it's the directory and we've included that
+df = df.drop(df.index[0])
 print(df)
