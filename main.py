b3d61506c215e9b17b8680e673e2ada38aed9150
Mac Nisbet <mfnisbet@lakeheadu.ca>
2022-11-24 21:12:38 -0500

    Audio classification model
    Full implementation of the audio classification model, also included some files to test with and a new batch_preprocess function to test them with.

--- main.py
+++ main.py
@@ -4,12 +4,20 @@
 # Import functions from another file
 from generate_dataframe import *
 from batch_preprocess import *
+import librosa
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from sklearn.preprocessing import LabelEncoder
 
 # Process all the audio files then generate a dataframe
 # Don't need to do this every time
+# !!!WARNING!!! Deletes files in new folder, then copies from old folder, if old folder is empty it will only delete
 batch_preprocess()
+batch_preprocess_test()
 # Generates the dataframe
 df = generate_dataframe()
 # Don't need the first value since it's the directory and we've included that
 df = df.drop(df.index[0])
 print(df)
+
